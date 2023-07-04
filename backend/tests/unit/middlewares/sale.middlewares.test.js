0d4041bc232a12f7b49fdf8c6f660b88342a8dc5
willianmenezess <willmenezes@outlook.com.br>
2023-07-04 11:55:34 -0300

    test: implementando testes em mais de 90% da aplicação

--- backend/tests/unit/middlewares/sale.middlewares.test.js
+++ backend/tests/unit/middlewares/sale.middlewares.test.js
@@ -23,5 +23,72 @@ describe('Realizando teste - SALES MIDDLEWARES:', function () {
     };
     await validationsInput.validateInputSale1(req, res, next);
     expect(next.called).to.be.equal(true);
+    await validationsInput.validateInputSale2(req, res, next);
+    expect(res.status.calledWith(400)).to.be.equal(true);
+    expect(res.status().json.calledWith({ message: '"productId" is required' })).to.be.equal(true);
     });
-  });  
+
+  it('Propriedade "quantity" da venda não informado', async function () {
+    const next = sinon.stub().returns();
+    const req = { body: [
+      {
+        productId: 1,
+      },
+      {
+        productId: 2,
+        quantity: 5,
+        },
+    ] };
+    const res = {
+      status: sinon.stub().returns({ json: sinon.stub().returns() }),
+    };
+    await validationsInput.validateInputSale1(req, res, next);
+    expect(next.called).to.be.equal(true);
+    await validationsInput.validateInputSale2(req, res, next);
+    expect(res.status.calledWith(400)).to.be.equal(true);
+    expect(res.status().json.calledWith({ message: '"quantity" is required' })).to.be.equal(true);
+    });
+
+  it('Propriedade "quantity" da venda com valor menor que 1', async function () {
+    const next = sinon.stub().returns();
+    const req = { body: [
+      {
+        productId: 1,
+        quantity: 0,
+      },
+      {
+        productId: 2,
+        quantity: 5,
+      },
+    ] };
+    const res = {
+      status: sinon.stub().returns({ json: sinon.stub().returns() }),
+    };
+    await validationsInput.validateInputSale1(req, res, next);
+    expect(next.called).to.be.equal(true);
+    await validationsInput.validateInputSale2(req, res, next);
+    expect(res.status.calledWith(422)).to.be.equal(true);
+    expect(res.status().json.calledWith({ message: '"quantity" must be greater than or equal to 1' })).to.be.equal(true);
+    });
+
+  it('propriedades da criação da venda passadas corretamente', async function () {
+    const next = sinon.stub().returns();
+    const req = { body: [
+    {
+      productId: 1,
+      quantity: 1,
+    },
+    {
+      productId: 2,
+      quantity: 5,
+    },
+    ] };
+    const res = {
+    status: sinon.stub().returns({ json: sinon.stub().returns() }),
+    };
+    await validationsInput.validateInputSale1(req, res, next);
+    expect(next.called).to.be.equal(true);
+    await validationsInput.validateInputSale2(req, res, next);
+    expect(next.called).to.be.equal(true);
+  });
+});  
